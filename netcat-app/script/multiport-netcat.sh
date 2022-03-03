97acaf44c37227736fd23bc9e7febe3bfe71f9c8
Rodrigo Moreira <moreira@zaia.com.br>
2022-03-02 21:05:28 -0300

    fix: script

--- netcat-app/script/multiport-netcat.sh
+++ netcat-app/script/multiport-netcat.sh
@@ -4,13 +4,6 @@ echo "Multi-Port Netcat."
 
 # TODO: input validation
 
-# echo "Verifying config file ports.ini."
-# number_regex='^[0-9]+$'
-# if ! [[ $(cat ports.ini) =~ number_regex ]]; then
-#    echo "ERROR: invalid config file. Ports should be in one line, separated by spaces. Finishing..."
-#    exit 1
-# fi
-
 echo "Configuration is OK."
 
 echo "Creating Netcat for each port"
@@ -20,10 +13,12 @@ for port in $(cat script/ports.ini); do
     pid=$(echo $!)
 
     # logic to validate if netcat really could be executed
-    live_process=$(ps -o pid= 992)
+    live_process=$(ps -o pid= $pid)
     if [[ "$live_process" == "" ]] ; then
         echo "ERROR: netcat was not able to bind to port $port. Finishing"
         exit 1
     fi
     echo "netcat binded to port $port. PID: $pid"
 done
+
+trap 'sleep infinity' EXIT
